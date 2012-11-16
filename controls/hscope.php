91ce4c4dc0f4a362d123f305e26f751addc5a888
aleksnick <aleksnick@list.ru>
2012-11-16 10:16:35 +0400

    удалил лишний код, оставил парсинг только с otvetplanet.ru

--- controls/hscope.php
+++ controls/hscope.php
@@ -15,7 +15,6 @@ function __construct() {
 }
 
 function getResponse() {
-	
 	if (file_exists(self::$path)) {
 		return self::getResponseString(self::getJsonFromFile());
 	}
@@ -24,10 +23,7 @@ function getResponse() {
 		self::saveJsonToFile();
 	}
 	return self::getResponseString(json_encode(self::$response));
-	
-	//return self::getSiteId($id);
-	//return self::getDays();
-	
+		
 }
 
 function getResponseString($s=null) {
@@ -41,7 +37,7 @@ function getResponseString($s=null) {
 }
 
 function getHscope() { $data=""; $ms=array();
-	if ((self::$htype=="ra-project") || (self::$htype=="astroscope") || (self::$htype=="otvetplanet")) {
+	if (self::$htype=="otvetplanet") {
 		for ($i=0;$i<12;$i++) {
 			$data=self::getDataFromSite(self::getUrl($i+1));
 			$ms=self::parseData($data);
@@ -53,49 +49,6 @@ function getHscope() { $data=""; $ms=array();
 
 function parseData($data) { $ms=array(); $ar=array();
 	if ($data) {
-		if (self::$htype=="ra-project") {
-			$parser=xml_parser_create();
-			xml_parser_set_option($parser,XML_OPTION_CASE_FOLDING,0);
-			xml_parser_set_option($parser,XML_OPTION_SKIP_WHITE,1);
-			xml_parse_into_struct($parser,$data,$values,$tags);
-			xml_parser_free($parser);
-			foreach ($tags as $key=>$val) {
-				if ($key=="item") {
-					for ($i=0;$i<count($val);$i+=2) {
-						$offset=$val[$i]+1;
-						$len=$val[$i+1]-$offset;
-						$ar=array_slice($values,$offset,$len);
-						for ($j=0;$j<count($ar);$j++) {
-							$ms[$ar[$j]["tag"]]=$ar[$j]["value"];
-						}					
-						$ms['description']=$ms['text']; unset($ms['text']);
-						return $ms;
-					}
-				}
-			}
-			return false;
-		}	
-		if (self::$htype=="astroscope") {
-			$parser=xml_parser_create();
-			xml_parser_set_option($parser,XML_OPTION_CASE_FOLDING,0);
-			xml_parser_set_option($parser,XML_OPTION_SKIP_WHITE,1);
-			xml_parse_into_struct($parser,$data,$values,$tags);
-			xml_parser_free($parser);
-			foreach ($tags as $key=>$val) {
-				if ($key=="item") {
-					for ($i=0;$i<count($val);$i+=2) {
-						$offset=$val[$i]+1;
-						$len=$val[$i+1]-$offset;
-						$ar=array_slice($values,$offset,$len);
-						for ($j=0;$j<count($ar);$j++) {
-							$ms[$ar[$j]["tag"]]=$ar[$j]["value"];
-						}
-						return $ms;
-					}
-				}
-			}
-			return false;
-		}
 		if (self::$htype=="otvetplanet") {
 			$data=preg_replace('/\r/','',$data);
 			$data=preg_replace('/\n/','',$data);
@@ -127,20 +80,8 @@ function getZodiacId($id) {
 }
 
 function getDataFromSite($url) { $data="";
-	if (self::$htype=="ra-project") {
-		file_get_contents("http://horoscope.ra-project.net/cron/import_rss.php");
-	}
 	if ($url) {
 		$data=file_get_contents($url);
-	}
-	if (self::$htype=="ra-project") {
-		if (preg_match('/date="(.*?)">/',$data,$dt,PREG_OFFSET_CAPTURE)) {
-			$date=strval($dt[1][0]);
-			console::write($date);
-			if (date("j.n.Y")==$date) {
-				self::$actual=true;
-			}
-		}
 	}
 	if (self::$htype=="otvetplanet") {
 		if (preg_match('/<div class="date-h" >(.*?)&nbsp;/',$data,$dt,PREG_OFFSET_CAPTURE)) {
@@ -155,25 +96,6 @@ function getDataFromSite($url) { $data="";
 }
 
 function getUrl($id=1) {
-	if (self::$htype=="ra-project") {
-		return "http://horoscope.ra-project.net/api/".$id;
-	}
-	if (self::$htype=="astroscope") {
-		switch ($id) {
-			case 1: return "http://astroscope.ru/rss_feed/aries.rss"; break;
-			case 2: return "http://astroscope.ru/rss_feed/taurus.rss"; break;
-			case 3: return "http://astroscope.ru/rss_feed/gemini.rss"; break;
-			case 4: return "http://astroscope.ru/rss_feed/cancer.rss"; break;
-			case 5: return "http://astroscope.ru/rss_feed/leo.rss"; break;
-			case 6: return "http://astroscope.ru/rss_feed/virgo.rss"; break;
-			case 7: return "http://astroscope.ru/rss_feed/libra.rss"; break;
-			case 8: return "http://astroscope.ru/rss_feed/scorpio.rss"; break;
-			case 9: return "http://astroscope.ru/rss_feed/sagittarius.rss"; break;
-			case 10: return "http://astroscope.ru/rss_feed/capricorn.rss"; break;
-			case 11: return "http://astroscope.ru/rss_feed/aquarius.rss"; break;
-			case 12: return "http://astroscope.ru/rss_feed/pisces.rss"; break;			
-		}
-	}
 	if (self::$htype=="otvetplanet") {
 		switch ($id) {
 			case 1: return "http://otvetplanet.ru/horoscopes/free/aries/".self::getSiteId($id)."/"; break;
